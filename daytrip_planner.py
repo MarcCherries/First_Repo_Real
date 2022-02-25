899475ce8cdcf12585fb676154f8a4022be5f206
MarcCherries <99926849+MarcCherries@users.noreply.github.com>
2022-02-25 11:25:40 -0800

    Update daytrip_planner.py
    Combined the 4 separate blocks of code that controlled the decision loops (are you satisfied with your selection or would you like to select again?)  for the individual choices in the trip planner into 4 reusable functions.

--- daytrip_planner.py
+++ daytrip_planner.py
@@ -24,6 +24,26 @@ def show_sel_rand (show_list):
    random_show = random.randint (0, len(show_list)-1)
    return show_list [random_show]
 
+def loc_init ():
+    decide_loc=(input(f'The LOCATION you will be travelling to is {loc_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
+    while decide_loc != '':
+        decide_loc = str(input(f'The LOCATION you will be travelling to is {loc_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
+
+def eat_init ():
+    decide_eat=(input(f'The EATERY you will be dining at is {eat_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
+    while decide_eat != '':
+        decide_eat = str(input(f'The EATERY you will be dining at is {eat_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
+
+def mode_init ():
+    decide_mode=(input(f'The MODE OF TRANSPORTATION you will be using is is {mode_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
+    while decide_mode != '':
+        decide_mode = str(input(f'The MODE OF TRANSPORTATION you will be using is {mode_res}], are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
+
+def show_init ():
+    decide_show=(input(f'The ENTERTAINMENT you will be enjoying is {show_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
+    while decide_show != '':
+        decide_show = str(input(f'The ENTERTAINMENT you will be enjoying is {show_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
+
 banner = ('***Powered by Radon Technologies (c) 2022  ****** WELCOME TO THE MARC CHERRIES TRIP PLANNER!****** Powered by Radon Technologies (c) 2022 ***')
 
 print (banner)
@@ -34,42 +54,26 @@ str(input('Please press ENTER to randomly select a LOCATION for your trip. '))
 while True:
     loc_res = loc_sel_rand(locations)
 
-    decide_loc=(input(f'The LOCATION you will be travelling to is {loc_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
-
-    while decide_loc != '':
-        loc_res = loc_sel_rand(locations)
-        decide_loc = str(input(f'The LOCATION you will be travelling to is {loc_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
-
+    loc_init()
+    
     str(input('Please press ENTER to randomly select an EATERY for your trip. '))
 
     eat_res = eat_sel_rand(eateries)
 
-    decide_eat = str(input(f'The EATERY you will be dining at is {eat_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect an EATERY, please press "R" '))
-
-    while decide_eat != '':
-        eat_res = eat_sel_rand(eateries)
-        decide_eat = str(input(f'The EATERY you will be dining at is {eat_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect an EATERY, please press "R" '))
+    eat_init()
 
     str(input('Please press ENTER to randomly select a MODE OF TRANSPORTATION for your trip. '))
 
     mode_res = mode_sel_rand(modes)
 
-    decide_mode = str(input(f'The MODE OF TRANSPORTATION you will be using is {mode_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a MODE OF TRANSPORTATION, please press "R" '))
-
-    while decide_mode != '':
-        mode_res = mode_sel_rand(modes)
-        decide_mode = str(input(f'The MODE OF TRANSPORTATION you will be using is {mode_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a MODE OF TRANSPORTATION, please press "R" '))
+    mode_init()
 
     str(input('Please press ENTER to randomly select ENTERTAINMENT for your trip. '))
 
     show_res = show_sel_rand(shows)
 
-    decide_show = str(input(f'The ENTERTAINMENT you will be seeing is {show_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a show, please press "R" '))
-
-    while decide_show != '':  
-        show_res = show_sel_rand(shows)
-        decide_show = str(input(f'The ENTERTAINMENT you will be seeing is {show_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect ENTERTRAINMENT, please press "R" '))
-
+    show_init()
+    
     readout = input('Okay! Looks like you have made some nice selections today.  Let us recap, shall we? Please press ENTER to continue ')
     if readout == '':
         print (f'LOCATION:                           {loc_res}') 
@@ -80,16 +84,9 @@ while True:
     restart = str(input('Does everything look good to you?  If you are satisfied with these selections, please press ENTER.  If you would like to START OVER, please press "X" '))
     if restart != '':  
         continue
-    #changing this to 'not equal empty string' instead of  == x made it work.  Need to learn more about this.
     
     print (f'Congratulations! Your trip is all set. You will be travelling to {loc_res} by way of {mode_res} and dining at the fabulous {eat_res}.  But the fun doesnt end there! You will cap the evening off with a private show preformed by none-other-than the great {show_res}! Have a great time!')
     break
 
-    #Questions for the instructors during code review:
-    #str() on inputs
-    #main while loop/break/continue
-    #whitespace
-    #.upper
-    #ascii art
-    #apostrophes in strings
+ 
     
