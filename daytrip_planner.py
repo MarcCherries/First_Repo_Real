cd064855ea7aa13f42a8e2d2785c2abfd1a32669
MarcCherries <99926849+MarcCherries@users.noreply.github.com>
2022-02-25 23:19:38 -0800

    Update daytrip_planner.py
    cut the code in half by combining 8 functions into 2.

--- daytrip_planner.py
+++ daytrip_planner.py
@@ -4,45 +4,23 @@ locations = ['New York', 'Los Angeles', 'Las Vegas', 'Milwaukee', 'Seattle']
 
 eateries = ['The Sticky Menu Diner', 'Mama Mia Italian Ristorante', 'Le Terible Vegan Restaurant', 'The Meat Wagon', 'Shanghai Surprise' ]
 
-modes = ['Ferrari', 'Limousine', 'Luxury Train', 'Helicopter', 'Schooner']
+modes = ['The Batmobile', 'Tauntaun', 'The USS Enterprise', 'TARDIS', 'That Huge Flying Dog-Thing From The Neverending Story']
 
-shows = ['Dave Chappelle', 'Joe Rogan', 'Neil Young', 'Barbara Streisand', 'Aaron Rodgers']
+shows = ['Wyld Stallyns', 'Sonic Death Monkey', 'Oneders', 'Uptown Girl: An 80s Joel Experience', 'Drive Shaft']
 
-def loc_sel_rand (loc_list):
-   random_loc = random.randint (0, len(loc_list)-1)
-   return loc_list [random_loc]
+#whew! I was able to cull this down to 2 functions from 8! It wasn't clicking for some reason at first but then I had an epiphany on how to do it while driving around
+def sel_rand (list):
+   random_sel = random.randint (0, len(list)-1)
+   return list [random_sel]
 
-def eat_sel_rand (eat_list):
-   random_eat = random.randint (0, len(eat_list)-1)
-   return eat_list [random_eat]
 
-def mode_sel_rand (mode_list):
-   random_mode = random.randint (0, len(mode_list)-1)
-   return mode_list [random_mode]
-
-def show_sel_rand (show_list):
-   random_show = random.randint (0, len(show_list)-1)
-   return show_list [random_show]
-
-def loc_init ():
-    decide_loc=(input(f'The LOCATION you will be travelling to is {loc_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
-    while decide_loc != '':
-        decide_loc = str(input(f'The LOCATION you will be travelling to is {loc_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
-
-def eat_init ():
-    decide_eat=(input(f'The EATERY you will be dining at is {eat_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
-    while decide_eat != '':
-        decide_eat = str(input(f'The EATERY you will be dining at is {eat_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
-
-def mode_init ():
-    decide_mode=(input(f'The MODE OF TRANSPORTATION you will be using is is {mode_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
-    while decide_mode != '':
-        decide_mode = str(input(f'The MODE OF TRANSPORTATION you will be using is {mode_res}], are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
-
-def show_init ():
-    decide_show=(input(f'The ENTERTAINMENT you will be enjoying is {show_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
-    while decide_show != '':
-        decide_show = str(input(f'The ENTERTAINMENT you will be enjoying is {show_res}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a LOCATION, please press "R" '))
+#I am so proud of this one below, it was quite a breakthrough and took over an hour!
+def dec_init (list, selection, list_type, list_verb):
+    decide_sel=(input(f'The {list_type} you will be {list_verb} is {selection}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a {list_type}, please press "R" '))
+    while decide_sel != '':
+        selection = sel_rand(list)
+        decide_sel = str(input(f'The {list_type} you will be {list_verb} is {selection}, are you satisfied with this selection?  If so, please press ENTER.  If you would like to reselect a {list_type}, please press "R" '))
+    return selection
 
 banner = ('***Powered by Radon Technologies (c) 2022  ****** WELCOME TO THE MARC CHERRIES TRIP PLANNER!****** Powered by Radon Technologies (c) 2022 ***')
 
@@ -52,35 +30,37 @@ str(input('Please press ENTER to randomly select a LOCATION for your trip. '))
 
 #start of continuous loop to allow user to restart application
 while True:
-    loc_res = loc_sel_rand(locations)
+    loc_res = sel_rand(locations)
 
-    loc_init()
+    loc_res = dec_init(locations, loc_res, 'LOCATION', 'travelling to')
     
     str(input('Please press ENTER to randomly select an EATERY for your trip. '))
 
-    eat_res = eat_sel_rand(eateries)
+    eat_res = sel_rand(eateries)
 
-    eat_init()
+    eat_res = dec_init(eateries, eat_res, 'EATERY', 'dining at')
 
     str(input('Please press ENTER to randomly select a MODE OF TRANSPORTATION for your trip. '))
 
-    mode_res = mode_sel_rand(modes)
+    mode_res = sel_rand(modes)
 
-    mode_init()
+    mode_res = dec_init(modes, mode_res, 'MODE OF TRANSPORTATION', 'using')
 
     str(input('Please press ENTER to randomly select ENTERTAINMENT for your trip. '))
 
-    show_res = show_sel_rand(shows)
+    show_res = sel_rand(shows)
 
-    show_init()
-    
+    show_res = dec_init(shows, show_res, 'ENTERTAINMENT', 'enjoying')
+
+    #possible function? only needed it once for this app
     readout = input('Okay! Looks like you have made some nice selections today.  Let us recap, shall we? Please press ENTER to continue ')
     if readout == '':
-        print (f'LOCATION:                           {loc_res}') 
+        print (f'LOCATION:                           {loc_res}')
         print (f'EATERY:                             {eat_res}') 
         print (f'MODE OF TRANSPORTATION:             {mode_res}') 
-        print (f'ENTERTRAINMENT:                     {show_res}') 
+        print (f'ENTERTAINMENT:                      {show_res}') 
 
+    #possible function? only needed it once for this app
     restart = str(input('Does everything look good to you?  If you are satisfied with these selections, please press ENTER.  If you would like to START OVER, please press "X" '))
     if restart != '':  
         continue
